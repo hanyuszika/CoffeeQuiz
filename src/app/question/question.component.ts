a116de4b91ee912c11ab8111c1feed09a1304987
Hanyu Krisztina <hanyu96@gmail.com>
2020-05-06 23:18:23 +0200

    button and style added

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -13,37 +13,47 @@ export class QuestionComponent implements OnInit {
   constructor(private questionService: QuestionService) { }
 
   isLoading: boolean = true;
-  remainingTime:any = 10;
+  remainingTime: any = 10;
+  countDownVisibility: boolean = true;
+
+  refresh(): void {
+    window.location.reload();
+  }
+
+  showAnswer() {
+    this.questionDetails.hidden = false;
+    this.countDownVisibility = false;
+  }
 
   ngOnInit(): void {
 
-      this.questionService.getQuestions().subscribe(
-        data => {
-          this.isLoading = false;
-          console.log(data[0]),
-            console.log("Question: " + data[0].question),
-            console.log("Category: " + data[0].category.title),
-            this.questionDetails = {
-              difficulty: data[0].value,
-              category: data[0].category.title,
-              question: data[0].question,
-              answer: data[0].answer,
-              airdate: data[0].airdate,
-              id: data[0].id,
-              hidden: true,
-            }
-        },
-        err => {
-          return console.log(err);
-        }
-      )
-      setTimeout(() => {this.questionDetails.hidden= false}, 10000);
-      var x = setInterval(() => {
-        this.remainingTime = this.remainingTime - 1;
-        if (this.remainingTime <= 0) {
-          clearInterval(x);
-          this.remainingTime = "Time out";
-        }
+    this.questionService.getQuestions().subscribe(
+      data => {
+        this.isLoading = false;
+        console.log(data[0]),
+          console.log("Question: " + data[0].question),
+          console.log("Category: " + data[0].category.title),
+          this.questionDetails = {
+            difficulty: data[0].value,
+            category: data[0].category.title,
+            question: data[0].question,
+            answer: data[0].answer,
+            airdate: data[0].airdate,
+            id: data[0].id,
+            hidden: true,
+          }
+      },
+      err => {
+        return console.log(err);
+      }
+    )
+    setTimeout(() => { this.questionDetails.hidden = false }, 10000);
+    var x = setInterval(() => {
+      this.remainingTime = this.remainingTime - 1;
+      if (this.remainingTime <= 0) {
+        this.countDownVisibility = false;
+        clearInterval(x);
+      }
     }, 1000);
   }
 }
