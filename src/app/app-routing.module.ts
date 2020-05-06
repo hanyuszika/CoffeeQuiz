c897347eef1ae7ba1a4207ff5b1f520b48b8f17a
Hanyu Krisztina <hanyu96@gmail.com>
2020-05-06 22:59:21 +0200

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuestionComponent} from './question/question.component';
+import { CategoryComponent } from './category/category.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: QuestionComponent },
+  { path: 'category', component: CategoryComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
